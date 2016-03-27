de5106f0a9179c43d091a1415c9913fcb07d2f42
cloverharvest <anna@schwabdev.com>
2016-03-27 01:33:45 -0700

    Adds html and styling

--- project00/js/app.js
+++ project00/js/app.js
@@ -1,1 +1,38 @@
 console.log("Let's race to Asher!");
+
+//Wait for document to load
+//Minimize globals
+
+var key1 = e.keyCode === 65;//for player1 move to right button
+});
+var key2 = e.keyCode === 76//for player2 move to right button
+});
+
+//define game variable and call game initializer
+
+$(document).ready(function(){
+
+});
+
+//Make Game constructor function adding the two players as
+//properties and a winner property
+
+//Add the initializer method to the Game prototype property
+//This listens to keypress events and then makes both tokens
+//move depending on keycode
+
+//Add the reset method to the Game prototype property
+//This moves back both players to the start of the racetrack
+
+//Make a Player Constructor function and create a player
+//name property
+
+//Adds the move method to the Player prototype property
+//This moves by x milliseconds the token's left margin X pixels
+//everytime its assigned keycode is pressed
+//and then if the token moves(and its left margin is greater or equal
+// than x pixels, then game stops and both token are
+//stopped from moving
+
+//A winner is declared and posted on messagebox(?)
+//Reset game
