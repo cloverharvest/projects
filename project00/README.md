0d4e6c3f1f2c9e4a598c04cc4f9b12a70e4784d8
cloverharvest <anna@schwabdev.com>
2016-03-27 21:41:45 -0700

    Contains working game

---
+++ project00/README.md
@@ -0,0 +1,8 @@
+README.md
+
+*Race Across the Bay
+This is a racing game between a ferry boat and a car across the Golden Gate Bridge to Marin County.
+The game can accommodate two players who are each assigned a key to push until their respective tokens reach the
+other side of the bay.
+It finishes with an alert window containing the winner information.
+Game is reset with a button.
--- project00/js/app.js
+++ project00/js/app.js
@@ -1,31 +1,49 @@
-console.log("Let's race to Asher!");
+console.log("Let's race to Marin!");
 
 //Wait for document to load
 //Minimize globals
 
-var key1 = e.keyCode === 65;//for player1 move to right button
-});
-var key2 = e.keyCode === 76//for player2 move to right button
-});
-
-//define game variable and call game initializer
+//Make a new Game object and assign it to a variable game
+//call game initializer within same function
 
 $(document).ready(function(){
-
+ game = new Game();
+ game.init();
 });
+
 
 //Make Game constructor function adding the two players as
 //properties and a winner property
 
+function Game() {
+  this.racer1 = new Player(1);
+  this.racer2 = new Player(2);
+  this.winner = null;
+}
+
+function Player(racerName) {
+  this.racerName = racerName;
+}
 //Add the initializer method to the Game prototype property
 //This listens to keypress events and then makes both tokens
 //move depending on keycode
+Game.prototype.init = function() {
+  var _this = this;
+  $(document).on('keyup', function(event) {
+    if (event.which === 65)
+    _this.racer1.move(1);
+    else if (event.which === 76)
+      _this.racer2.move(2);
+  });
+};
 
 //Add the reset method to the Game prototype property
 //This moves back both players to the start of the racetrack
+Game.prototype.reset = function() {
+  $("#racer1").css("margin-left", 0);
+  $("#racer2").css("margin-left", 0);
+};
 
-//Make a Player Constructor function and create a player
-//name property
 
 //Adds the move method to the Player prototype property
 //This moves by x milliseconds the token's left margin X pixels
@@ -34,5 +52,19 @@ $(document).ready(function(){
 // than x pixels, then game stops and both token are
 //stopped from moving
 
-//A winner is declared and posted on messagebox(?)
+//A winner is declared and posted via an alert box
 //Reset game
+Player.prototype.move = function(token) {
+  $('#racer' + token).animate({
+    'margin-left': '+=30px'
+  }, 100);
+    if (parseInt($('#racer' + token).css('margin-left')) >= 780) {
+      $(document).off();
+
+      $('#racer1').stop();
+      $('#racer2').stop();
+      alert("Race Winner: " + (token === 1) ? "Racer " + token + " wins! You reached Marin safely!" : "Racer " + token + " wins! Your reached Marin safely!");
+
+      setTimeout(function(){game.reset()}, 300);
+  }
+};
